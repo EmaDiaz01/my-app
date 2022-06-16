04012bead6811a25b970896425db622770735194
EmaDiaz01 <demanuel269@gmail.com>
2022-06-16 19:32:38 -0300

    agregar ruta salida

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>res.send('HOLA MUNDO!!'))
+app.get('/salida', (req, res)=>res.send('Chau! '))
+
+app.listen(3030, ()=>console.log('server running in port 3030'))
