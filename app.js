337d2c288998b9777cdf8dd17d9346aa847f5d3e
EmaDiaz01 <demanuel269@gmail.com>
2022-06-19 19:37:39 -0300

    agregue daniel

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res)=>res.send('HOLA MUNDO!!'))
+app.get('/', (req,res)=>res.send('HOLA DANIEL!!'))
 app.get('/salida', (req, res)=>res.send('Chau! '))
 
 app.listen(3030, ()=>console.log('server running in port 3030'))
